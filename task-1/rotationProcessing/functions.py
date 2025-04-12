ba61a74b6387d329c861cd0185006711afcb6807
Miguel Lima <up202108659@up.pt>
2025-04-12 18:39:11 +0100

    sift fix all working

--- task-1/rotationProcessing/functions.py
+++ task-1/rotationProcessing/functions.py
@@ -20,12 +20,10 @@ def flann_matcher(data, descriptors1, descriptors2="descriptors", matchesTitle="
     index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
     search_params = dict(checks=50)  # or pass empty dictionary
     flann = cv2.FlannBasedMatcher(index_params, search_params)
-    matches = flann.knnMatch(data["metadata"][descriptors1], data["metadata"][descriptors2], k=2)
+    matches = flann.match(data["metadata"][descriptors1], data["metadata"][descriptors2])
 
-    # Store all matches (taking the best match from each pair)
-    good = [m for m, _ in matches]
 
-    data["metadata"][matchesTitle] = good
+    data["metadata"][matchesTitle] = matches
     return data
 
 #apply RANSAC to estimate homography from matches
